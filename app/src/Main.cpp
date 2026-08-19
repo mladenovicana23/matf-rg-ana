33210effa5e8814a48f036233323803fa2c54382
Ana Mladenovic <anam230304@gmail.com>
2026-08-19 18:14:03 +0200

    Create basic application structure

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -3,6 +3,15 @@
 /**
  * Start here...
  */
-int main(int argc, char **argv) {
-    return 0;
-}
+
+class MainController final : public engine::core::Controller {};
+
+class MainApp final : public engine::core::App {
+protected:
+    void app_setup() override {
+        auto main_controller = register_controller<MainController>();
+        main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+    }
+};
+
+int main(int argc, char **argv) { return std::make_unique<MainApp>()->run(argc, argv); }
